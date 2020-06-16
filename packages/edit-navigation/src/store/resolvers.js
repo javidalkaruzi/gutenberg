0fe0292ede44e4b6854e53657e2566483b5dbadd
Adam Zieliński <adam@adamziel.com>
2020-06-16 15:31:43 +0200

    Simplify navigationPost resolver

--- packages/edit-navigation/src/store/resolvers.js
+++ packages/edit-navigation/src/store/resolvers.js
@@ -11,28 +11,11 @@ import { createBlock } from '@wordpress/blocks';
 /**
  * Internal dependencies
  */
-import { resolveSelect, select, dispatch } from './controls';
+import { resolveSelect, dispatch } from './controls';
 import { KIND, POST_TYPE, buildNavigationPostId } from './utils';
 
 export function* getNavigationPost( menuId ) {
-	const postQuery = { id: `navigation-post-${ menuId }` };
-	const postResolved = yield select(
-		'hasFinishedResolution',
-		POST_TYPE,
-		postQuery
-	);
-
-	if ( postResolved ) {
-		return yield select(
-			'getEditedEntityRecord',
-			KIND,
-			POST_TYPE,
-			buildNavigationPostId( menuId )
-		);
-	}
-
 	const menuItemsQuery = { menus: menuId, per_page: -1 };
-
 	const menuItems = yield resolveSelect( 'getMenuItems', menuItemsQuery );
 
 	const [ navigationBlock, menuItemIdToClientId ] = createNavigationBlock(
